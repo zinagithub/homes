198b6b12de73d868622c6369fe4dc59b2cca402d
Talaa Zina <talazina@gmail.com>
2019-11-25 19:21:49 +0100

    Updates

---
+++ precache-manifest.c9580b05e4efd8361bdce9526ba44bbf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1094c0f625438ba9b5b1707ca10274c3",
+    "url": "/homes/index.html"
+  },
+  {
+    "revision": "ba9e1db420b4d42f9a00",
+    "url": "/homes/static/css/main.34ae3acf.chunk.css"
+  },
+  {
+    "revision": "049c406559fa102b67f9",
+    "url": "/homes/static/js/2.8e583783.chunk.js"
+  },
+  {
+    "revision": "ba9e1db420b4d42f9a00",
+    "url": "/homes/static/js/main.ff648605.chunk.js"
+  },
+  {
+    "revision": "ee4afb52c6348dcc745e",
+    "url": "/homes/static/js/runtime-main.f0959815.js"
+  },
+  {
+    "revision": "27c60d992b23e7c3f505705bbf928afd",
+    "url": "/homes/static/media/austin-distel-uPK2TbJlvMQ-unsplash (1).27c60d99.jpg"
+  },
+  {
+    "revision": "78ce056d17154e39bfa23bd99918315c",
+    "url": "/homes/static/media/jason-briscoe-AQl-J19ocWE-unsplash.78ce056d.jpg"
+  },
+  {
+    "revision": "39ce3ff8d2e62b97eca649921c5aec5d",
+    "url": "/homes/static/media/jesse-roberts-561igiTyvSk-unsplash.39ce3ff8.jpg"
+  },
+  {
+    "revision": "b08ddcb1f057434bd532185223255251",
+    "url": "/homes/static/media/patrick-perkins-3wylDrjxH-E-unsplash.b08ddcb1.jpg"
+  },
+  {
+    "revision": "edb502fc3b6f05886bfae736bac14e09",
+    "url": "/homes/static/media/ralph-kayden-2d4lAQAlbDA-unsplash.edb502fc.jpg"
+  },
+  {
+    "revision": "92145339b4a9edf8472b7563623777f8",
+    "url": "/homes/static/media/rayyu-maldives-photographer-Qv1HnAeoqHs-unsplash.92145339.jpg"
+  }
+]);
